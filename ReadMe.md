cf1fe2530b87e3a2f8471b84cb493295d13df396
deepa <deepawadkar2002@gmail.com>
2024-02-27 11:50:11 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+### File upload API
+
+
+## 1. create packege.json
+   npm init -y
+
+## 2. npm packages
+    npm i --save express cors dotenv mongoose multer
+
+multer -> express middleware used to upload files 
+express-fileupload ->
+
+
+## 3. http-status codes
+    npm i --save http-status-codes
---
+++ controller/fileCtrl.js
@@ -0,0 +1,42 @@
+const { StatusCodes } = require('http-status-codes')
+
+//upload File - post
+const uploadFile =async (req,res) => {
+    try {
+        res.status(StatusCodes.CREATED).json({ msg : 'upload file'})
+    } catch (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status:false, msg : err})
+        
+    }
+}
+//read all files - get
+const readAllFiles =async (req,res) => {
+    try {
+        res.status(StatusCodes.ACCEPTED).json({ msg : 'read all files'})
+    } catch (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status:false, msg : err})
+        
+    }
+}
+
+//read single files - get(id)
+const readSingleFile =async (req,res) => {
+    try {
+        res.status(StatusCodes.ACCEPTED).json({ msg : 'read single file'})
+    } catch (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status:false, msg : err})
+        
+    }
+}
+
+//read delete files - delete(id)
+const deleteFile =async (req,res) => {
+    try {
+        res.status(StatusCodes.ACCEPTED).json({ msg : 'Delete file'})
+    } catch (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({status:false, msg : err})
+        
+    }
+}
+
+module.exports = {uploadFile , readAllFiles, readSingleFile, deleteFile}

3b28c1aba4e3ee7cdb64f9f8698bb8bc2c05bf53
deepa <deepawadkar2002@gmail.com>
2024-03-05 12:15:37 +0530

    first commit

--- controller/fileCtrl.js
+++ controller/fileCtrl.js
@@ -25,7 +25,10 @@ const uploadFile =async (req,res) => {
 //read all files - get
 const readAllFiles =async (req,res) => {
     try {
+        // file is exists in dsb or not
         let data = await File.find({})
+        
+        //final response
         res.status(StatusCodes.ACCEPTED).json({ status:true, length: data.length, files:data})
     } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status:false, msg : err})
@@ -36,11 +39,17 @@ const readAllFiles =async (req,res) => {
 //read single files - get(id)
 const readSingleFile =async (req,res) => {
     try {
+        //reading file id from router parameter
         let id = req.params.id
+
+        // file is exists in dsb or not
         let extFile = await File.findById(id)
+
+        //if file not exists -> throw err
         if(!extFile) 
         return res.status(StatusCodes.NOT_FOUND).json({status : false, msg: 'requested id not found'})
 
+        //final response
         res.status(StatusCodes.ACCEPTED).json({ status: true , file:extFile })
     } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status:false, msg : err})
@@ -51,17 +60,27 @@ const readSingleFile =async (req,res) => {
 //read delete files - delete(id)
 const deleteFile =async (req,res) => {
     try {
+        //reading file id from router parameter
         let id = req.params.id
+
+        // file is exists in dsb or not
         let extFile = await File.findById(id)
+
+        //if file not exists -> throw err
         if(!extFile)
         return res.status(StatusCodes.NOT_FOUND).json({ status: false, msg :'requested id not found'})
         
-        await fs.unlinkSync(extFile.path) //delete the physical file from the server
+        //delete the physical file from the server
+        fs.unlinkSync(extFile.path) 
 
+        //deleting db connect
         await File.findByIdAndDelete(id)
 
-        res.status(StatusCodes.ACCEPTED).json({ status: true , msg : ' file Deleted successfully'})
+        //final response
+        return res.status(StatusCodes.ACCEPTED).json({ status: true , msg : ' file Deleted successfully'})
     } catch (err) {
+
+        //locical error
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({status:false, msg : err})
         
     }
